0eba5715a094e222cc8c3622018e973b50684c69
Irene Elizabeth Sabu <irene@cet.ac.in>
2023-11-11 17:48:56 +0530

    Initial Commit

---
+++ Controllers/ConfigurationController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO.Ports;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WaveMaster_Backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ConfigurationController : ControllerBase
+    {
+        // GET: api/<ConfigurationController>
+        [HttpGet]
+        public IEnumerable<string> GetPortName()
+        {
+            var ports = SerialPort.GetPortNames();
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ConfigurationController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ConfigurationController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ConfigurationController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ConfigurationController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/WaveMasterDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WaveMaster_Backend.Models
+{
+    public class WaveMasterDbContext : DbContext
+    {
+        public WaveMasterDbContext(DbContextOptions<WaveMasterDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
