41cb755a6b240bcca0ecf0ce2d51a486e70e0bdd
sanjay <sanjaysunil34@gmail.com>
2023-11-13 14:44:21 +0530

    Stash error update

--- Controllers/ConfigurationController.cs
+++ Controllers/ConfigurationController.cs
@@ -27,7 +27,6 @@ namespace WaveMaster_Backend.Controllers
 
         // POST api/<ConfigurationController>
         [HttpPost]
-<<<<<<< Updated upstream
         public void Post(ConenctionParamsModel value)
         {
             Console.WriteLine($"Port Name : {value.portName}");
@@ -35,11 +34,6 @@ namespace WaveMaster_Backend.Controllers
             Console.WriteLine($"Data Bit : {value.dataBit}");
             Console.WriteLine($"Baud Rate : {value.baudRate}");
             Console.WriteLine($"Parity : {value.parity}");
-=======
-        public void Post(Object value)
-        {
-            Console.WriteLine($"Port Name : {value}");
->>>>>>> Stashed changes
         }
 
         // PUT api/<ConfigurationController>/5
