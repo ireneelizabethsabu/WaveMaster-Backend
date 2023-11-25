39083c30e307ec7a27ddff4fd7537e11c48b3469
Irene Elizabeth Sabu <irene@cet.ac.in>
2023-11-25 17:54:00 +0530

    byte by byte reading

--- Services/ReadService.cs
+++ Services/ReadService.cs
@@ -58,17 +58,18 @@ namespace WaveMaster_Backend.Services
         public async void DataReceivedHandler()
         {
             SerialPort sp = _sharedVariableService.serialPort;
+            int flag = 0;
             while (sp.IsOpen)
-            {
-                var buffer = new byte[1024];
+            {                
                 try
-                {  
+                {
+                    var buffer = new byte[1024];
                     if (sp.BytesToRead == 0)
                         continue;
 
-                    int cnt = sp.BaseStream.Read(buffer, 0, 1024);
-                    string asciiString = Encoding.ASCII.GetString(buffer, 0, cnt);
-                    Console.WriteLine(asciiString);
+                    int bytesRead = sp.BaseStream.Read(buffer, 0, 1024);
+                    string asciiString = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    //Console.WriteLine(asciiString);
                     if(asciiString.Contains("Capture Stopped;"))
                     {
                         Log.Information("Capture Stopped; received");
@@ -76,6 +77,7 @@ namespace WaveMaster_Backend.Services
                         Mode = "";
                     }else if (asciiString.Contains("DATA"))
                     {
+                        Log.Information("Peak to peak and frequency data; received");
                         await _hub.Clients.All.SendAsync("fetchData", asciiString);
                     }
                     else if (asciiString.Contains("Capture Started;"))
@@ -85,68 +87,102 @@ namespace WaveMaster_Backend.Services
                     }
                     else
                     {
+                        var dataBuffer = new byte[1024];
+                        var indexOfSync = 0;
+                        var lengthOfBuffer = bytesRead;
+                        bool sync = false;
+                        var byteStore = new byte[2];
+                        if (asciiString.Contains("SYNC;"))
+                        {
+                            sync = true;
+                            indexOfSync = asciiString.IndexOf(";SYNC;");
+                            Log.Information($"SYNC read at index {indexOfSync}");
+                            Array.Copy(buffer, 0, dataBuffer, 0, indexOfSync);
+                            lengthOfBuffer = indexOfSync;
+                        }
+                        else
+                        {
+                            sync = false;
+                            dataBuffer = buffer;
+                            lengthOfBuffer = bytesRead;
+                        }
                         if (Mode.Equals("CAPTURE"))
                         {
-                            for (int i = 0; i < cnt; i++)
+                            if (flag == 1)
                             {
-                                if ((char)buffer[i] == ';')
+                                byteStore[1] = dataBuffer[0];
+                                int data = BitConverter.ToUInt16(byteStore, 0);
+                                //Console.Write(data);
+                                PlotData pd = new PlotData();
+                                pd.voltage = data * (3.3 / 4096);
+                                pd.time = DateTime.Now;
+                                dataStore.Add(pd);
+                            }
+                            for (int i = flag; i < lengthOfBuffer ; i+=2)
+                            {                                
+                                if(i+1 < lengthOfBuffer)
                                 {
-                                    Console.WriteLine("semiiii");
+                                    flag = 0;
+                                    //var temp = dataBuffer[i];
+                                    //dataBuffer[i] = dataBuffer[i + 1];
+                                    //dataBuffer[i + 1] = temp;
+                                    int data = BitConverter.ToUInt16(dataBuffer, i);
+                                    //Console.Write(data);
+                                    PlotData pd = new PlotData();
+                                    pd.voltage = data * (3.3/4096);
+                                    pd.time = DateTime.Now;
+                                    dataStore.Add(pd);
+                                    Console.WriteLine($"{pd.voltage} - {pd.time} ");
                                 }
                                 else
                                 {
-                                    Console.Write((int)buffer[i]);
+                                    flag =  1;
+                                    byteStore[0] = dataBuffer[i];
+                                }
+
+                                if(dataStore.Count() > 200)
+                                {
+                                    NotifyObservers();
+                                    dataStore.Clear();
                                 }
                             }
-                            Console.WriteLine();
+                            if(sync)
+                            {
+                                for (int i = indexOfSync + 6 ; i < bytesRead; i = i + 2)
+                                {
+                                    //Console.WriteLine((char)buffer[indexOfSync + 5]);
+                                    if (i + 1 < bytesRead)
+                                    {
+                                        flag = 0;
+                                        //var temp = buffer[i];
+                                        //buffer[i] = buffer[i + 1];
+                                        //buffer[i + 1] = temp; 
+                                        int data = BitConverter.ToUInt16(buffer, i);
+                                        PlotData pd = new PlotData();
+                                        pd.voltage = data * (3.3 / 4096);
+                                        pd.time = DateTime.Now;
+                                        dataStore.Add(pd);
+                                        Console.WriteLine($"{pd.voltage} - {pd.time} ");
+                                    }
+                                    else
+                                    {
+                                        flag = 1;
+                                    }
+                                    if (dataStore.Count() > 200)
+                                    {
+                                        NotifyObservers();
+                                        dataStore.Clear();
+                                    }
+                                }
+                            }                               
                         }
                     }        
                 }
                 catch (Exception ex)
                 {
                     Debug.WriteLine($"{DateTime.Now.ToString("HH:mm:ss:fff")} RxCommandsAsync: Exception {ex}");
-                }
-                //Console.WriteLine(ReceivedString);
-                //byte[] buffer = new byte[2];
-                //if (Mode.Equals("CAPTURE"))
-                //{
-                //    try
-                //    {
-                //        sp.Read(buffer, 0, 2);
-                //        string buf = Encoding.ASCII.GetString(buffer);
-                //        Console.WriteLine(buf);
-                //        if (buf.Equals("Ca"))
-                //        {
-                //            Console.WriteLine("HELLLLLLLLLLLLLLLLLLLLLLLLLLLOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-                //        }
-                //        int hexData = BitConverter.ToInt16(buffer, 0);
-                //        Console.WriteLine(hexData);
-                //        //string hexData = BitConverter.ToString(buffer).Replace("-", "");
-                //        //string hexData = BitConverter.ToString(buffer).Replace("-", "");
-                //        PlotData pd = new PlotData();
-                //        pd.voltage = hexData * (3.3 / 4096);
-                //        pd.time = DateTime.Now;
-                //        dataStore.Add(pd);
-                //        Console.WriteLine($"{pd.voltage} - {pd.time} ");
-
-                //        if (dataStore.Count() > 200)
-                //        {
-                //            //_hub.Clients.All.SendAsync("transferPlotData", dataStore);
-                //            NotifyObservers();
-                //            dataStore.Clear();
-                //        }
-                //    }
-                //    catch (Exception ex)
-                //    {
-                //        //Mode = "READ";
-                //        //_hub.Clients.All.SendAsync("captureControl", "STOP CAPTURE");
-                //        //ReceivedString = sp.ReadTo(";");
-                //        //Console.WriteLine(ReceivedString);
-                //    }
-                //}
-                
-            }
-            
+                }       
+            }            
         }
         public void NotifyObservers()
         {
