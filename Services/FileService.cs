f3d99bc000530f2703a05a95649022f133c51f08
Irene Elizabeth Sabu <irene@cet.ac.in>
2023-11-27 17:52:30 +0530

    commented generate controller

---
+++ Services/FileService.cs
@@ -0,0 +1,29 @@
+﻿using WaveMaster_Backend.Observers;
+
+namespace WaveMaster_Backend.Services
+{
+    public interface IFileService
+    {
+        public void FileWrite();
+        public void FileRead();
+    }
+    public class FileService
+    {
+        public void FileWrite()
+        {
+            try
+            {
+                string jsonString = JsonSerializer.Serialize(signalData);
+                string filePath = "settings.json";
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        public void FileRead() 
+        { 
+            
+        }
+    }
+}
